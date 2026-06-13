74fee392385d4b28112289d8991f4b90d578faca
Guilherme Macedo <guilherme@kioskefutebolclube.com.br>
2026-06-13 16:43:05 -0300

    Adicionando root layout do Nextjs

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'KFC 2026',
+  description: 'Sistema de Gestão de Fardamento',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  )
+}
